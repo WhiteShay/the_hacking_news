444e2e6b7e819692fa9a8eedc9c0a3202dd37b95
hnassri <46187243+hnassri@users.noreply.github.com>
2019-01-30 21:56:43 +0100

    ajout coms

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,5 @@
+#C'est la class de la table comment. Ici on y lie la table comment avec la table user, link et secondary_comment
+
 class Comment < ApplicationRecord
   belongs_to :link
   belongs_to :user
