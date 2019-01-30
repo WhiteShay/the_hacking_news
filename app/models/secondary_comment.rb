288d24d4aa50b71bc2791c9fe8f85e0c85d4d888
hnassri <46187243+hnassri@users.noreply.github.com>
2019-01-30 21:59:11 +0100

    ajout coms

--- app/models/secondary_comment.rb
+++ app/models/secondary_comment.rb
@@ -1,3 +1,5 @@
+# C'est la class de la table secondarycomment. Ici on y lie la table secondarycomment avec la table user et comment
+
 class SecondaryComment < ApplicationRecord
   belongs_to :user
   belongs_to :comment
