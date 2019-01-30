4a4cc7db5ad7241fda098d4f5390f73825e7b9ab
hnassri <46187243+hnassri@users.noreply.github.com>
2019-01-30 21:54:50 +0100

    ajout coms

--- db/migrate/20190130184206_add_index_to_secondary_comments.rb
+++ db/migrate/20190130184206_add_index_to_secondary_comments.rb
@@ -1,3 +1,5 @@
+# On lit la table secondarycomment avec la table user et la table comment
+
 class AddIndexToSecondaryComments < ActiveRecord::Migration[5.2]
   def change
     add_reference :secondary_comments, :comment, foreign_key: true
