cea6c96fd17ea9c68720d54885cbe7faebb902f0
hnassri <46187243+hnassri@users.noreply.github.com>
2019-01-30 21:53:01 +0100

    ajout coms

--- db/migrate/20190130183115_add_index_to_comments.rb
+++ db/migrate/20190130183115_add_index_to_comments.rb
@@ -1,3 +1,5 @@
+#On lie la table comment avec la table user et la table link
+
 class AddIndexToComments < ActiveRecord::Migration[5.2]
   def change
     add_reference :comments, :link, foreign_key: true
